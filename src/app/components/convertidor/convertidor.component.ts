8d17cfd399a9bcf9c552a38a692a0d552c75a5c8
miryambathilde <mbathilde@gmail.com>
2021-05-06 19:12:32 +0200

    termino de arreglar la aplicacion

--- src/app/components/convertidor/convertidor.component.ts
+++ src/app/components/convertidor/convertidor.component.ts
@@ -7,20 +7,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConvertidorComponent implements OnInit {
   cantidad = 0;
-  monedaActual = 'EUR';
+  monedaActual = 'EURO';
   monedaConvertir = 'USD';
   total = 0;
-  monedas: string[] = ['USD', 'EUR', 'LIBRA']
+  monedas: string[] = ['USD', 'EURO', 'LIBRA']
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  //metodo CONVERTIR
   convertir() {
-    console.log('llamada al metodo');
+    switch (this.monedaActual) {
+      case 'USD':
+        if(this.monedaConvertir === 'USD') {
+          this.total = this.cantidad;
+        }
+        if(this.monedaConvertir === 'EURO') {
+          this.total = this.cantidad * 0.83 // dollar to euro
+        }
+        if(this.monedaConvertir === 'LIBRA') {
+          this.total = this.cantidad * 0.72 //dollar to libra
+        }
+        break;
+      case 'EURO':
+        if(this.monedaConvertir === 'EURO') {
+          this.total = this.cantidad;
+        }
+        if(this.monedaConvertir === 'USD') {
+          this.total = this.cantidad * 1.21 // euro to dollar
+        }
+        if(this.monedaConvertir === 'LIBRA') {
+          this.total = this.cantidad * 0.87 //euro to libra
+        }
+        break;
+      case 'LIBRA':
+        if(this.monedaConvertir === 'LIBRA') {
+          this.total = this.cantidad;
+        }
+        if(this.monedaConvertir === 'USD') {
+          this.total = this.cantidad * 1.39 // libra to dollar
+        }
+        if(this.monedaConvertir === 'EURO') {
+          this.total = this.cantidad * 1.15 //libra to euro
+        }
+        break;
+    }
   }
-
-
 }
